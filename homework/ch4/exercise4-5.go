f450571f5f288d35cfd27f018308366e8173e723
Yuicon <910722178@qq.com>
2017-09-14 15:12:37 +0800

    fix:4-5

--- homework/ch4/exercise4-5.go
+++ homework/ch4/exercise4-5.go
@@ -5,19 +5,19 @@ import (
 )
 
 func main() {
-	a := []string{"m", "a", "a", "x"}
+	a := []string{"m", "m", "a", "a", "a", "x", "x"}
 	a = noRepetition(a)
 	fmt.Println(a)
 }
 
 func noRepetition(s []string) []string {
-	for i := 0; i < len(s)-1; i++ {
-		if s[i] == s[i+1] {
-			for j := i + 1; j < len(s)-1; j++ {
-				s[j] = s[j+1]
-				s = s[:len(s)-1]
-			}
+	var a []string
+	var tmp string
+	for _, t := range s {
+		if tmp != t {
+			a = append(a, t)
 		}
+		tmp = t
 	}
-	return s
+	return a
 }
