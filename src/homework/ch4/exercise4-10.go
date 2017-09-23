8febf7487f534aa01812a2af1dfce645065fccd6
Yuicon <910722178@qq.com>
2017-09-24 00:16:11 +0800

    4-10

--- src/homework/ch4/exercise4-10.go
+++ src/homework/ch4/exercise4-10.go
@@ -3,9 +3,10 @@ package main
 
 import (
 	"fmt"
+	"homework/ch4/github"
 	"log"
 	"os"
-	"homework/ch4/github"
+	"time"
 )
 
 func main() {
@@ -15,7 +16,18 @@ func main() {
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
+		sinceDay := time.Since(item.CreatedAt).Hours() / 24
+		fmt.Println(sinceDay)
 		fmt.Printf("#%-5d %9.9s %.55s\n",
 			item.Number, item.User.Login, item.Title)
+		if sinceDay < 30 {
+			fmt.Println("under month\n ")
+			continue
+		}
+		if sinceDay > 365 {
+			fmt.Println("more year\n ")
+			continue
+		}
+		fmt.Println("under year\n ")
 	}
 }
